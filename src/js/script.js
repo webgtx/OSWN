c89bc96bff08af5317ad89a53d8d3e472fb07617
webgtx <dxv1d@tuta.io>
2021-03-12 02:44:44 +0200

    added menu, and fixed animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-@@include('info.js')
+const menuButton = document.getElementById('menu');
+const menuBlock = document.querySelector('.intro__menu');
+const exitButton = document.querySelector('.exit-button')
+const mainBlock = document.querySelector('.intro__main');
+
+menuButton.addEventListener('click', () => {
+    if (menuButton.classList.contains('menuBtn-active')) {
+        menuButton.classList.remove('menuBtn-active');
+        menuBlock.classList.remove('menuBtn-active');
+    } else {
+        menuButton.classList.add('menuBtn-active');
+        menuBlock.classList.add('menuBtn-active');
+    }
+});
+exitButton.addEventListener('click', () => {
+    if (menuButton.classList.contains('menuBtn-active')) {
+        menuButton.classList.remove('menuBtn-active');
+        menuBlock.classList.remove('menuBtn-active');
+    }
+});
